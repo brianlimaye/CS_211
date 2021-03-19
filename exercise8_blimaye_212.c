e36d0160d1eebef8178c74783b57e9f533857643
blimay3581c <BLimaye@softwaresecurityconsultants.com>
2021-03-18 20:15:07 -0400

    Add files via upload
    Bahar's Changes

--- exercise8_blimaye_212.c
+++ exercise8_blimaye_212.c
@@ -4,18 +4,23 @@
 void clean_up(char ** names, int * ages, int n);
 
 void clean_up(char ** names, int * ages, int n) {
-
 	int i;
 
-	for(i = 0; i < n; i++) {
+	if (names != NULL)
+	{
+		for(i = 0; i < n; i++) {
 
-		free(*names);
-		*names = NULL;
-		names++;
+			free(*names);
+			*names = NULL;
+			names++;
+		}
 	}
 
-	free(ages);
-	ages = NULL;
+	if (ages != NULL)
+	{
+		free(ages);
+		ages = NULL;
+	}
 }
 
 int main() {
@@ -23,85 +28,91 @@ int main() {
 	//Problem 1
 	int i;
 	int n;
+	int char_count;
+	int exit_code = 1;
+
+	char ** names = NULL;
+	int * ages = NULL;
+
 	printf("Please enter in the number of students to be recorded. ");
-	scanf("%d", &n);
+	
+	char_count = scanf("%d", &n);
+
 
 	if(n < 0) {
-
 		printf("Invalid input...");
-		exit(0);
+		goto exit;
 	}
 
-	char ** names = (char **) malloc(n * sizeof(char *));
+	names = (char **) malloc(n * sizeof(char *));
 
 	if(names == NULL) {
-
 		printf("malloc failed...");
-		exit(0);
+		goto exit;
 	}
-	int * ages = (int *) malloc(n * sizeof(int));
+
+	ages = (int *) malloc(n * sizeof(int));
 
 	if(names == NULL) {
-
 		printf("malloc failed...");
+		goto exit;
 	}
 
 	for(i = 0; i < n; i++) {
-
 		names[i] = (char *) malloc(15 * sizeof(char));
 		printf("Please enter in student #%d's name. ", i + 1);
-		scanf("%s", names[i]);
+		char_count = scanf("%14s", names[i]);
+	
 
 		printf("Please enter in student #%d's age. ", i + 1);
-		scanf("%d", &(*(ages + i)));
+		char_count = scanf("%d", &(*(ages + i)));
+		
 	}
 
 	for(i = 0; i < n; i++) {
-
 		printf("Student %d's name is: %s, age: %d\n", i + 1, names[i], *(ages + i));
 	}
 
 	//Problem 2
 
-	int m;
+	int m = -1;
 	printf("Please enter in any additional number of students that still need to be recorded. ");
-	scanf("%d", &m);
+	char_count = scanf("%d", &m);
 
 	if(m < 0) {
-
 		printf("Invalid input...");
-		exit(0);
+		goto exit;
 	}
 
 	names = (char **) realloc(names, (n + m) * sizeof(char *));
 
 	if(names == NULL) {
-
 		printf("realloc failed...");
-		exit(0);
+		goto exit;
 	}
 	ages = (int *) realloc(ages, (n + m) * sizeof(int));
 
 	if(ages == NULL) {
-
 		printf("realloc failed...");
-		exit(0);
+		goto exit;
 	}
 
 	for(i = n; i < n + m; i++) {
-
 		names[i] = (char *) malloc(15 * sizeof(char));
 		printf("Please enter in student #%d's name. ", i + 1);
-		scanf("%s", names[i]);
+		char_count = scanf("%14s", names[i]);
 
 		printf("Please enter in student #%d's age. ", i + 1);
-		scanf("%d", &(*(ages + i)));
+		char_count = scanf("%d", &(*(ages + i)));
 	}
 
 	for(i = 0; i < (m + n); i++) {
-
 		printf("Student %d's name is: %s, age: %d\n", i + 1, names[i], *(ages + i));
 	}
 
+	exit_code = 0;
+
+	exit:
 	clean_up(names, ages, n + m);
+	exit(exit_code);
 }
